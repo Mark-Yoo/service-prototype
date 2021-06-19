6de021ab1999b2286382d3aac05e7d021d6c26cc
Mark Yoo <yjinhyuk@gmail.com>
2021-06-19 19:53:33 +0900

    Update: redux-devtools, redux, react-redux, thunk, applied

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import CommonHeader from './components/Header';
 import ServicePage from './pages/ServicePage';
 import SignUp from './pages/SignUp';
+import { useSelector } from 'react-redux';
+import SignIn from './pages/SignIn';
 
 function App() {
+  const { token } = useSelector((state) => state.postInfo);
+
   return (
     <>
       <CommonHeader />
       <Route path="/" component={ServicePage} exact={true} />
-      <Route path="/login" component={ServicePage} />
-      <Route path="/sign-up" component={SignUp} />
+      <Route path="/login" component={SignIn}>
+        {token && <Redirect to="/" />}
+      </Route>
+      <Route path="/sign-up" component={SignUp}>
+        {token && <Redirect to="/" />}
+      </Route>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './assets/styles/global-styles';
 import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk)),
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <GlobalStyle />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
