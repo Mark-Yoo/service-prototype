b65dbb986fa40634d2a0a7defd5b3ccf1f79f6bf
Mark Yoo <yjinhyuk@gmail.com>
2021-06-19 19:44:09 +0900

    Add: redux react-redux applied

---
+++ src/modules/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import postInfo from './postInfo';
+
+const rootReducer = combineReducers({ postInfo });
+
+export default rootReducer;
---
+++ src/modules/postInfo.js
@@ -0,0 +1,112 @@
+import { handleActions } from 'redux-actions';
+import { signUp, signIn } from '../lib/api';
+
+const initialState = {
+  loading: {
+    GET_USER: false,
+  },
+  token: null,
+  statusCode: null,
+};
+
+const SIGN_UP_INFO = 'postInfo/SIGN_UP_INFO';
+const SIGN_UP_INFO_SUCCESS = 'postInfo/SIGN_UP_INFO_SUCCESS';
+const SIGN_UP_INFO_FAILURE = 'postInfo/SIGN_UP_INFO_FAILURE';
+
+const SIGN_IN_INFO = 'postInfo/SIGN_IN_INFO';
+const SIGN_IN_INFO_SUCCESS = 'postInfo/SIGN_IN_INFO_SUCCESS';
+const SIGN_IN_INFO_FAILURE = 'postInfo/SIGN_IN_INFO_FAILURE';
+
+// const SIGN_OUT_INFO = 'postInfo/SIGN_OUT_INFO';
+// const SIGN_OUT_INFO_SUCCESS = 'postInfo/SIGN_OUT_INFO_SUCCESS';
+// const SIGN_OUT_INFO_FAILURE = 'postInfo/SIGN_OUT_INFO_FAILURE';
+
+export const getTokenSignUp = (payload) => async (dispatch) => {
+  dispatch({ type: SIGN_UP_INFO });
+  try {
+    const response = await signUp(payload);
+    dispatch({
+      type: SIGN_UP_INFO_SUCCESS,
+      payload: response.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: SIGN_UP_INFO_FAILURE,
+      payload: e,
+      error: true,
+    });
+    throw e;
+  }
+};
+
+export const getTokenSignIn = (payload) => async (dispatch) => {
+  dispatch({ type: SIGN_IN_INFO });
+  try {
+    const response = await signIn(payload);
+    dispatch({
+      type: SIGN_IN_INFO_SUCCESS,
+      payload: response,
+    });
+  } catch (e) {
+    dispatch({
+      type: SIGN_IN_INFO_FAILURE,
+      payload: e,
+      error: true,
+    });
+    throw e;
+  }
+};
+
+const postInfo = handleActions(
+  {
+    [SIGN_UP_INFO]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USER: true,
+      },
+    }),
+    [SIGN_UP_INFO_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USER: false,
+      },
+      token: action.payload,
+    }),
+    [SIGN_UP_INFO_FAILURE]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USER: false,
+      },
+    }),
+    [SIGN_IN_INFO]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USER: true,
+      },
+    }),
+    [SIGN_IN_INFO_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USER: false,
+      },
+      token: action.payload.data,
+      statusCode: null,
+    }),
+    [SIGN_IN_INFO_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USER: false,
+      },
+      statusCode: action.payload.message.includes(401) && 401,
+    }),
+  },
+  initialState,
+);
+
+export default postInfo;
