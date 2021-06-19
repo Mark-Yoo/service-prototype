7f7f2f093721846e6e5281d2a75beb98dc98f0ba
Mark Yoo <yjinhyuk@gmail.com>
2021-06-19 18:22:04 +0900

    Update: signin basic submit function is now set

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import {
+  LoginInput,
+  LoginForm,
+  LoginBtn,
+  BtnWrapper,
+  LoginInputLabel,
+} from './styles';
+import { FormWrapper, InputWrapper } from '../SignUp/styles';
+import { useInput } from '../../hooks/useInput';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTokenSignIn } from '../../modules/postInfo';
 
 const SignIn = () => {
-  return null;
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const { statusCode } = useSelector((state) => state.postInfo);
+  const dispatch = useDispatch();
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      dispatch(getTokenSignIn({ email: email, password: password }));
+    },
+    [email, password, dispatch],
+  );
+
+  return (
+    <>
+      <FormWrapper>
+        <LoginForm onSubmit={onSubmit}>
+          <InputWrapper>
+            <LoginInputLabel htmlFor="user-login-email">이메일</LoginInputLabel>
+            <LoginInput
+              name="user-login-email"
+              type="email"
+              value={email}
+              onChange={onChangeEmail}
+              required
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <LoginInputLabel htmlFor="user-login-password">
+              비밀번호
+            </LoginInputLabel>
+            <LoginInput
+              name="user-login-password"
+              type="password"
+              value={password}
+              onChange={onChangePassword}
+              required
+            />
+          </InputWrapper>
+          <BtnWrapper>
+            <LoginBtn type="onSubmit">로그인</LoginBtn>
+          </BtnWrapper>
+        </LoginForm>
+      </FormWrapper>
+      {statusCode === 401 && <alert>비밀번호를 확인해주세요</alert>}
+    </>
+  );
 };
 
 export default SignIn;
---
+++ src/pages/signin/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const LoginForm = styled.form`
+  width: 100%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  margin: 0 auto;
+  background-color: gold;
+`;
+
+export const LoginInputLabel = styled.label`
+  width: 30%;
+  display: flex;
+  align-items: center;
+`;
+
+export const LoginInput = styled.input`
+  width: 70%;
+  height: 30px;
+  border: 1px solid black;
+`;
+
+export const BtnWrapper = styled.div`
+  display: flex;
+  margin: 0 auto;
+`;
+
+export const LoginBtn = styled.button`
+  width: 200px;
+  height: 30px;
+  margin: 10px;
+`;
